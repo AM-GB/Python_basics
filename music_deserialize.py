af2f65c566a49034f275e6cb5795a013844c7226
AM <anatolminor91@inbox.ru>
2020-04-25 17:06:25 +0300

    work with json. and pickle.

---
+++ music_deserialize.py
@@ -0,0 +1,13 @@
+import json
+import pickle
+
+with open('group.json', 'r', encoding='utf-8') as f:
+    result = json.load(f)
+print(result)
+print(type(result))
+
+with open('group_pickle', 'rb') as f:
+    result = pickle.load(f)
+
+print(result)
+print(type(result))
